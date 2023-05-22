a66cd7f6416533f9c3c38a43e5823a743d4b37ae
Yousef <yalmulla@deakin.edu.au>
2023-05-22 19:21:12 +0530

    Add Google map and create get location Button

--- app/src/main/java/com/application/lostandfound/service/CreateAdvert.java
+++ app/src/main/java/com/application/lostandfound/service/CreateAdvert.java
@@ -1,22 +1,47 @@
 package com.application.lostandfound.service;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.application.lostandfound.MainActivity;
 import com.application.lostandfound.R;
 import com.application.lostandfound.db.DBHandler;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class CreateAdvert extends AppCompatActivity {
 
-    // creating variables for our edittext, button and dbhandler
-    private EditText nameEdt, phoneEdt, descriptionEdt, dateEdt, locationEdt;
-    private Button createAdvertBtn;
+    // creating variables for our edittext, button and dbHandler
+    public EditText nameEdt, phoneEdt, descriptionEdt, dateEdt, locationEdt;
+    private Button createAdvertBtn, getLocationBtn;
     private DBHandler dbHandler;
+    private FusedLocationProviderClient fusedLocationClient;
+    private GoogleMap googleMap;
+
+    // Request code for location permission request
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +55,23 @@ public class CreateAdvert extends AppCompatActivity {
         dateEdt = findViewById(R.id.productDateEditText);
         locationEdt = findViewById(R.id.productLocationEditText);
         createAdvertBtn = findViewById(R.id.createAdvertBtn);
+        getLocationBtn = findViewById(R.id.getLocationBtn);
 
-        // creating a new dbhandler class
+        // creating a new dbHandler class
         // and passing our context to it.
         dbHandler = new DBHandler(CreateAdvert.this);
+
+        // initializing our fusedLocationClient.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // adding on click listener to our button.
+
+        getLocationBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(CreateAdvert.this, GetLocation.class);
+            startActivity(intent);
+
+        });
+
 
         // adding on click listener to our button.
         createAdvertBtn.setOnClickListener(v -> {
---
+++ app/src/main/java/com/application/lostandfound/service/GetLocation.java
@@ -0,0 +1,129 @@
+package com.application.lostandfound.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.application.lostandfound.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class GetLocation extends AppCompatActivity implements OnMapReadyCallback {
+
+    // creating variables
+    private GoogleMap googleMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Button getLocationBtn;
+    private MapView mapView;
+
+    // Request code for location permission request
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_location);
+
+
+        // initializing all our variables.
+        getLocationBtn = findViewById(R.id.getLocationBtn);
+        mapView = findViewById(R.id.mapView);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Initialize the map view
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        // On click listener for button
+        getLocationBtn.setOnClickListener(v -> {
+            // calling a method to get the location.
+            getLocationBtn.setEnabled(false);
+
+            // Check if the location permission has been granted
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    // Handle the current location update
+                                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+                                    double latitude = location.getLatitude();
+                                    double longitude = location.getLongitude();
+                                    LatLng currentLocation = new LatLng(latitude, longitude);
+
+
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Intent intent = new Intent(GetLocation.this, CreateAdvert.class);
+                                            intent.putExtra("location", currentLocation.toString());
+                                            startActivity(intent);
+                                        }
+                                    },5000);
+                                }
+                            }
+                        });
+            } else {
+                // Request permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        googleMap.setMinZoomPreference(15);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(41.015137, 28.979530)));
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
